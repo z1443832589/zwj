b49a9851255a199f645917dd5474ba53c9973073
zwj144 <1443832589@qq.com>
2019-07-24 14:20:40 +0800

    first commit

--- js/deal.js
+++ js/deal.js
@@ -1,3 +1,8 @@
 $('.header img:first-child').on('touchend',function(){
 	location.href="play.html"
 })
+var data = JSON.parse(localStorage.getItem('dataPeople'))
+$('.id h1').text(data[0].id)
+$('.show').on('touchend',function(){
+	$(this).text('隐藏身份')
+})
