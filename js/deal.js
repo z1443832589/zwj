e7c9003a7cce2636f0ed2a8eb74a7ab3fb87b18a
zwj144 <1443832589@qq.com>
2019-07-24 18:15:32 +0800

    play等文件实现新功能

--- js/deal.js
+++ js/deal.js
@@ -3,6 +3,58 @@ $('.header img:first-child').on('touchend',function(){
 })
 var data = JSON.parse(localStorage.getItem('dataPeople'))
 $('.id h1').text(data[0].id)
-$('.show').on('touchend',function(){
-	$(this).text('隐藏身份')
+var bool = true;
+var i = 0;
+Prev()
+$('.button').on('click','button',function(e){
+	if($(this).attr('class') == 'show'){
+		if(bool){
+			$(this).text('隐藏身份')
+			$('.bg').addClass(data[i].identity)
+			bool = false;
+		}else{
+			Remove()
+		}
+	}else if($(this).attr('class') == 'next'){
+		var num = Object.getOwnPropertyNames(data).length;
+		Remove()
+		i++;    //获取总人数
+		if(i >= num - 1){
+			if(i>=num){
+				location.href = "playGame.html"
+			}else{
+				$('.id h1').text(i+1)
+				$(this).text('开始游戏');
+			}
+		}else{
+			$(this).text('下一个')
+			$('.id h1').text(i+1)
+		}
+		Prev()
+	}
+	else if($(this).attr('class') == 'prev'){
+		Remove()
+		$('.next').text('下一个')
+		i--;
+		if(i>=0){
+			$('.id h1').text(i+1)
+			if(i>0){
+				$(this).removeAttr('disabled','disabled')
+			}else{
+				$(this).attr('disabled','disabled')
+			}
+		}
+	}
 })
+function Prev(){
+	if(i<=0){
+		$('.prev').attr('disabled','disabled')
+	}else{
+		$('.prev').removeAttr('disabled','disabled')
+	}
+}
+function Remove(){
+	$('.show').text('显示身份')
+	$('.bg').removeClass(data[i].identity)
+	bool = true;
+}
